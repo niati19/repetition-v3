2362add5857782f0667fd0961813e981ae10e343
niati19 <90190320+niati19@users.noreply.github.com>
2022-01-17 12:59:28 +0100

    jättebra

--- Jättebraprogram.py
+++ Jättebraprogram.py
@@ -1,8 +1,17 @@
-from tkinter import N
-gris = 300
-fågel = 500
+grispris = input("Hur mycket kostar en gris? ")
+int(grispris)
 
-if fågel > gris: 
+fågelpris = input("Hur mycket kostar en fågel? ")
+int(fågelpris)
+
+if fågelpris > grispris: 
     print("Ja, en fågel är dyrare än en gris")
-    print(f"En fågel kostar {fågel} kr")
-    print(f"En gris kostar {gris} kr")  
+    print(f"En fågel kostar {fågelpris} kr")
+    print(f"En gris kostar {grispris} kr")  
+
+elif grispris > fågelpris:
+    print("En gris kostar mer än en fågel")
+    print(f"En gris kostar {grispris} kr")
+    print(f"En fågel kostar {fågelpris} kr")
+elif grispris == fågelpris:
+    print(f"De kostar lika mycket, alltså {grispris} kr")
