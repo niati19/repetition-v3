39925cec8becf90ba1c25f299640e6110b91584d
niati19 <90190320+niati19@users.noreply.github.com>
2022-01-17 12:46:32 +0100

    verygood

---
+++ Jättebraprogram.py
@@ -0,0 +1,8 @@
+from tkinter import N
+gris = 300
+fågel = 500
+
+if fågel > gris: 
+    print("Ja, en fågel är dyrare än en gris")
+    print(f"En fågel kostar {fågel} kr")
+    print(f"En gris kostar {gris} kr")  
